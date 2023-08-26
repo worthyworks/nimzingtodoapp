75504844a5a5b38c959f7767d10229862ce5524e
Nimzing Ladep <info@worthy-works.com>
2023-08-26 09:31:36 +0100

    file paths to data

--- models.py
+++ models.py
@@ -1,43 +1,51 @@
+import os
 import sqlite3
 
-def create_table():
-    connection = sqlite3.connect('todos.db')
+def get_db_path(app):
+    return os.path.join(app.root_path, 'todos.db')
+
+def create_table(app):
+    db_path = get_db_path(app)
+    connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
     cursor.execute('CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)')
+    connection.commit()
+    connection.close()
+
+def add_todo(app, todo):
+    db_path = get_db_path(app)
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO todos (content) VALUES (?)', (todo,))
     connection.commit()
     connection.close()
     
 
 
 
-def add_todo(todo):
-    connection = sqlite3.connect('todos.db')
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO todos (content) VALUES (?)', (todo,))
-    connection.commit()
-    connection.close()
 
 
-def get_todos():
-    connection = sqlite3.connect('todos.db')
+
+def get_todos(app):
+    db_path = get_db_path(app)
+    connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM todos')
     todos = cursor.fetchall()
     connection.close()
     return todos
 
-
-
-
-def delete_todo(todo):
-    connection = sqlite3.connect('todos.db')
+def delete_todo(app, todo):
+    db_path = get_db_path(app)
+    connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
     cursor.execute('DELETE FROM todos WHERE content = ?', (todo,))
     connection.commit()
     connection.close()
 
-def update_todo(old_todo, new_todo):
-    connection = sqlite3.connect('todos.db')
+def update_todo(app, old_todo, new_todo):
+    db_path = get_db_path(app)
+    connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
     cursor.execute('UPDATE todos SET content = ? WHERE content = ?', (new_todo, old_todo))
     connection.commit()
